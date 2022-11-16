1f73eecec046c949879384ec83f4c2e4ca967a66
Jake Westerfield <jecobwesterfield@gmail.com>
2022-11-16 16:44:48 -0500

    adds in html to view running server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/test", (req, res) => {
+    res.send("<h1>It's working 🤗</h1>")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
