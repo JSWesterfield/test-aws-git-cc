bfa3b09d0a4b2aade40f63e69d898ae7ff0798cc
Jake Westerfield <jecobwesterfield@gmail.com>
2022-11-16 16:55:43 -0500

    adds in /test path

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+// const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("<h1>It's working 🤗, from root dir /</h1>")
 })
 
 app.get("/test", (req, res) => {
-    res.send("<h1>It's working 🤗</h1>")
+    res.send("<h1>It's working 🤗 from /test</h1>")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
